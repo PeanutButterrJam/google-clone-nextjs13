fb9a98afd7c4e9440db0aac28435daa77acea30b
PeanutButterrJam <mollyyyho102@gmail.com>
2023-06-10 15:32:19 +0800

    added loading effect

---
+++ src/app/search/web/loading.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div>
+        <div className='mx-2 pt-10 m-w-6xl lg:pl-52 animate-pulse'>
+            <div className="h-25 w-48 bg-gray-200 mb-2.5"> </div>
+            <div className="h-3.5 w-[360px] bg-gray-200 mb-2.5"> </div>
+            <div className="h-2 max-w-[560px]bg-gray-200 mb-2.5"> </div>
+            <div className="h-2 max-w-[530px] bg-gray-200 mb-2.5"> </div>
+        </div>
+        <div className='mx-2 pt-10 m-w-6xl lg:pl-52 animate-pulse'>
+            <div className="h-25 w-48 bg-gray-200 mb-2.5"> </div>
+            <div className="h-3.5 w-[360px] bg-gray-200 mb-2.5"> </div>
+            <div className="h-2 max-w-[560px]bg-gray-200 mb-2.5"> </div>
+            <div className="h-2 max-w-[530px] bg-gray-200 mb-2.5"> </div>
+        </div>
+        <div className='mx-2 pt-10 m-w-6xl lg:pl-52 animate-pulse'>
+            <div className="h-25 w-48 bg-gray-200 mb-2.5"> </div>
+            <div className="h-3.5 w-[360px] bg-gray-200 mb-2.5"> </div>
+            <div className="h-2 max-w-[560px]bg-gray-200 mb-2.5"> </div>
+            <div className="h-2 max-w-[530px] bg-gray-200 mb-2.5"> </div>
+        </div>
+         <div className='mx-2 pt-10 m-w-6xl lg:pl-52 animate-pulse'>
+            <div className="h-25 w-48 bg-gray-200 mb-2.5"> </div>
+            <div className="h-3.5 w-[360px] bg-gray-200 mb-2.5"> </div>
+            <div className="h-2 max-w-[560px]bg-gray-200 mb-2.5"> </div>
+            <div className="h-2 max-w-[530px] bg-gray-200 mb-2.5"> </div>
+        </div>
+        </div>
+  )
+}
