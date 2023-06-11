89c606edb6348c0147f3a574fdb824fac5601419
PeanutButterrJam <mollyyyho102@gmail.com>
2023-06-11 11:09:26 +0800

    added margin for loading page

--- src/app/search/image/loading.jsx
+++ src/app/search/image/loading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function loading() {
   return (
     <div className="pt-10 mx-2 lg:pl-52 max-w-6xl space-x-4 flex">
-      <div className="animate-pulse justify-center">
+      <div className="animate-pulse ml-[100px] sm:ml-0">
         <div className="h-48 w-48 mb-4 bg-gray-200 rounded-md"></div>
         <div className="h-2 w-48 mb-2.5 bg-gray-200 rounded-md"></div>
         <div className="h-2 w-44 mb-2.5 bg-gray-200 rounded-md"></div>
